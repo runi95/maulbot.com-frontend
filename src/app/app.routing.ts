aec0fcfaf97eeba7aeca38379160ed26f7c036b8
Henning <henning.ber@gmail.com>
2018-05-04 17:25:04 +0200

    routing to home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
 
 
 // import {HomeComponent} from './home/home.component'
 const appRoutes: Routes = [
     {
-      // path: '',
-      // component : HomeComponent,
+      path: '',
+      component : AppComponent,
     },
 
   ];
