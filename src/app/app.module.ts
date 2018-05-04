c4e37f525170b9d8210532519bd215d87c1f5792
runi95 <rse005@uib.no>
2018-05-04 22:30:07 +0200

    Finished part 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,26 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import {MessageService} from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
