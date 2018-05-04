6da7beff5a10e7bdb40ba8f341d3c3c2a8ca96b3
Henning <henning.ber@gmail.com>
2018-05-04 18:00:36 +0200

    bootstrap is in da game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import {Component, Input, NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-@NgModule({
-  imports: [NgbModule]
-})
+
 export class AppComponent {
   title = 'Tour of Heroes';
   @Input()
